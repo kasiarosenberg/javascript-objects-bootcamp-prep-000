6ab3432e99a7ac1b0a8b5dc96e5343fe13b9965f
kate ROSENBERG <kate@digitalmachinations.com>
2018-02-27 10:30:44 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {}
+function updatePlaylist(playlist, artistName, song){
+  return Object.assign(playlist, {[artistName]: song});
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
+  return playlist;
+}
